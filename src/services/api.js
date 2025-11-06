e5e50f30977708b03260dde746a49d7210835cba
Marcos Salles <marcowsgm@gmail.com>
2025-11-06 16:55:04 -0300

    [NEW] Add API methods for fetching and creating grupos

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ const API_URL = 'http://localhost:3001/api';
 export const contatosApi = {
     getAll: async () => {
         const response = await fetch(`${API_URL}/contatos`);
+        if (!response.ok) throw new Error("Erro ao buscar contatos");
         return response.json();
     },
 
@@ -12,6 +13,7 @@ export const contatosApi = {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(contato)
         });
+        if (!response.ok) throw new Error("Erro ao criar contato");
         return response.json();
     },
 
@@ -21,6 +23,7 @@ export const contatosApi = {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(contato)
         });
+        if (!response.ok) throw new Error("Erro ao atualizar contato");
         return response.json();
     },
 
@@ -28,9 +31,11 @@ export const contatosApi = {
         const response = await fetch(`${API_URL}/contatos/${id}`, {
             method: 'DELETE'
         });
+        if (!response.ok) throw new Error("Erro ao deletar contato");
         return response.json();
     }
 };
+
 
 export const mensagensApi = {
     enviar: async (grupo, mensagem) => {
@@ -42,3 +47,21 @@ export const mensagensApi = {
         return response.json();
     }
 };
+
+export const gruposApi = {
+    getAll: async () => {
+        const res = await fetch("http://localhost:3001/api/grupos");
+        if (!res.ok) throw new Error("Erro ao buscar grupos");
+        return res.json();
+    },
+    create: async (grupo) => {
+        const res = await fetch("http://localhost:3001/api/grupos", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(grupo)
+        });
+        return res.json();
+    }
+};
+
+// TODO: MODULARIZAR SERVICES
