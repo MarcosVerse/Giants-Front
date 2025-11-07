8b3144fba9b1e60299d4e01c0b0aac67ee5e95a0
Marcos Salles <marcowsgm@gmail.com>
2025-11-07 16:53:11 -0300

    [CHANGE] Color of group buttons

--- src/components/Contatos/ContatoModal.jsx
+++ src/components/Contatos/ContatoModal.jsx
@@ -49,7 +49,7 @@ export default function ContatoModal({
                             value={formData.telefone}
                             onChange={(e) => onChange({ ...formData, telefone: e.target.value })}
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-transparent"
-                            placeholder="5541999999999"
+                            placeholder="+55 (41) 9999-9999"
                         />
                     </div>
 
@@ -72,10 +72,10 @@ export default function ContatoModal({
 
                                             onChange({ ...formData, grupo: novos });
                                         }}
-                                        className={`px-3 py-1 rounded-full text-sm border transition
+                                        className={`px-3 py-1 rounded-full text-sm  transition shadow-md
                         ${ativo
-                                                ? "bg-orange-500 text-white border-orange-500"
-                                                : "bg-gray-100 text-gray-700 border-gray-300 hover:bg-gray-200"
+                                                ? "bg-orange-500 text-white border-orange-500 shadow-orange-500/50"
+                                                : "bg-gray-50 text-gray-700 hover:bg-gray-200"
                                             }`}
                                     >
                                         {g.nome}
